065b7179ca425e9963ad9ffc34f1dcc820ed7512
Gabriel Jose Sanches <gabriel.sanches@sp.senai.br>
2025-09-05 13:45:28 -0300

    finished login view

--- spectral_project/spectral_app/forms.py
+++ spectral_project/spectral_app/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import AuthenticationForm
 from .models import *
+from django import forms
 
 
 class CustomAuthenticationForm(AuthenticationForm):
@@ -7,3 +8,4 @@ class CustomAuthenticationForm(AuthenticationForm):
         "invalid_login": "As credenciais fornecidas são inválidas!",
         "inactive": "Sua conta está inativa!",
     }
+
